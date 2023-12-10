ed425ebb807f66f87b88930960a74929683f94c8
tatermysalad <rosette.rural0c@icloud.com>
2023-12-10 10:13:20 +0100

    feat: added testing for space controller

--- test/spaceController.test.js
+++ test/spaceController.test.js
@@ -7,12 +7,11 @@ const {
   getDatabaseURL,
 } = require('../src/database');
 
+const {deleteUserByEmail} = require('../src/functions/userFunctions');
 const {
-  getAllUsers,
-  createUser,
-  deleteUserByEmail,
-  generateUserJWT,
-} = require('../src/functions/userFunctions');
+  createSpace,
+  generateAccessCode,
+} = require('../src/functions/spaceFunctions');
 
 // Ensure the database is connected before all tests
 beforeAll(async () => {
@@ -25,6 +24,7 @@ beforeEach(async () => {
   const delayDuration = 2000; // implement timeout
   await new Promise((resolve) => setTimeout(resolve, delayDuration));
 
+  // Delete user before each test
   const emailsToDelete = ['bob.johnson@example.com'];
   for (email of emailsToDelete) {
     await deleteUserByEmail(email);
@@ -40,7 +40,7 @@ describe('Space Router', () => {
   describe('GET /spaces', () => {
     test('should return an array of spaces', async () => {
       // Register test user
-      await request(app).post('/users/register').send({
+      const registerResponse = await request(app).post('/users/register').send({
         first_name: 'Bob',
         last_name: 'Johnson',
         email: 'bob.johnson@example.com',
@@ -56,31 +56,197 @@ describe('Space Router', () => {
       });
 
       const jwt = await loginResponse.body.jwt;
+
+      const invite_code = await generateAccessCode();
+
+      // Create a test space
+      const spaceDetails = {
+        admin_id: registerResponse.body.user._id,
+        user_ids: [],
+        name: 'Test Space',
+        description: 'Test space description',
+        capacity: 10,
+        invite_code: invite_code,
+      };
+
+      const createdSpace = await createSpace(spaceDetails);
+
       // Make a request to the endpoint with the JWT in the headers
-      const response = await request(app)
-        .get('/spaces')
-        .set('jwt', jwt);
+      const response = await request(app).get('/spaces').set('jwt', jwt);
 
       // Assertions
       expect(response.status).toBe(200);
-      expect(response.body.spaceCount).toBeGreaterThanOrEqual(0);
+      expect(response.body.spaceCount).toBeGreaterThanOrEqual(1); // Assuming the created space is included
       expect(response.body.spaces).toBeInstanceOf(Array);
     });
   });
 
   describe('GET /spaces/:spaceID', () => {
-    // Your test for GET /spaces/:spaceID route
+    test('should return details of a specific space', async () => {
+      const registerResponse = await request(app).post('/users/register').send({
+        first_name: 'Bob',
+        last_name: 'Johnson',
+        email: 'bob.johnson@example.com',
+        password: 'password123',
+        post_code: '54321',
+        country: 'NZ',
+        position: 'Developer',
+      });
+
+      const loginResponse = await request(app).post('/users/login').send({
+        email: 'bob.johnson@example.com',
+        password: 'password123',
+      });
+
+      const jwt = loginResponse.body.jwt;
+
+      const invite_code = await generateAccessCode();
+
+      const spaceDetails = {
+        admin_id: registerResponse.body.user._id,
+        user_ids: [],
+        name: 'Test Space',
+        description: 'Test space description',
+        capacity: 10,
+        invite_code: invite_code,
+      };
+
+      const createdSpace = await createSpace(spaceDetails);
+
+      const response = await request(app)
+        .get(`/spaces/${createdSpace._id}`)
+        .set('jwt', jwt);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject(spaceDetails);
+    });
   });
 
   describe('POST /spaces', () => {
-    // Your test for POST /spaces route
+    test('should create a new space', async () => {
+      const registerResponse = await request(app).post('/users/register').send({
+        first_name: 'Bob',
+        last_name: 'Johnson',
+        email: 'bob.johnson@example.com',
+        password: 'password123',
+        post_code: '54321',
+        country: 'NZ',
+        position: 'Developer',
+      });
+
+      const loginResponse = await request(app).post('/users/login').send({
+        email: 'bob.johnson@example.com',
+        password: 'password123',
+      });
+
+      const jwt = loginResponse.body.jwt;
+
+      const spaceDetails = {
+        user_ids: [],
+        name: 'New Test Space',
+        description: 'New test space description',
+        capacity: 15,
+      };
+
+      const response = await request(app)
+        .post('/spaces')
+        .set('jwt', jwt)
+        .send(spaceDetails);
+
+      expect(response.status).toBe(200);
+      expect(response.body.space).toMatchObject(spaceDetails);
+    });
   });
 
   describe('PUT /spaces/:spaceID', () => {
-    // Your test for PUT /spaces/:spaceID route
+    test('should update details of a specific space', async () => {
+      const registerResponse = await request(app).post('/users/register').send({
+        first_name: 'Bob',
+        last_name: 'Johnson',
+        email: 'bob.johnson@example.com',
+        password: 'password123',
+        post_code: '54321',
+        country: 'NZ',
+        position: 'Developer',
+      });
+
+      const loginResponse = await request(app).post('/users/login').send({
+        email: 'bob.johnson@example.com',
+        password: 'password123',
+      });
+
+      const jwt = loginResponse.body.jwt;
+      // add invite code as not using the POST route
+      const invite_code = await generateAccessCode();
+
+      const spaceDetails = {
+        admin_id: registerResponse.body.user._id,
+        user_ids: [],
+        name: 'Test Space',
+        description: 'Test space description',
+        capacity: 10,
+        invite_code: invite_code,
+      };
+
+      const createdSpace = await createSpace(spaceDetails);
+
+      const updatedSpaceDetails = {
+        user_ids: [],
+        name: 'Updated Test Space',
+        description: 'Updated test space description',
+        capacity: 20,
+      };
+
+      const response = await request(app)
+        .put(`/spaces/${createdSpace._id}`)
+        .set('jwt', jwt)
+        .send(updatedSpaceDetails);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject(updatedSpaceDetails);
+    });
   });
 
   describe('DELETE /spaces/:spaceID', () => {
-    // Your test for DELETE /spaces/:spaceID route
+    test('should delete a specific space', async () => {
+      const registerResponse = await request(app).post('/users/register').send({
+        first_name: 'Bob',
+        last_name: 'Johnson',
+        email: 'bob.johnson@example.com',
+        password: 'password123',
+        post_code: '54321',
+        country: 'NZ',
+        position: 'Developer',
+      });
+
+      const loginResponse = await request(app).post('/users/login').send({
+        email: 'bob.johnson@example.com',
+        password: 'password123',
+      });
+
+      const jwt = loginResponse.body.jwt;
+      // add invite code as not using the POST route
+      const invite_code = await generateAccessCode();
+
+      const spaceDetails = {
+        admin_id: registerResponse.body.user._id,
+        user_ids: [],
+        name: 'Test Space',
+        description: 'Test space description',
+        capacity: 10,
+        invite_code: invite_code,
+      };
+
+      const createdSpace = await createSpace(spaceDetails);
+
+      const response = await request(app)
+        .delete(`/spaces/${createdSpace._id}`)
+        .set('jwt', jwt);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject({
+        message: 'Space deleted successfully',
+      });
+    });
   });
 });

ef28a08de5cc1f60fcf6a5861519e46f9741f31c
tatermysalad <rosette.rural0c@icloud.com>
2023-12-11 23:56:44 +0100

    feat: add CRUD tests for bookings

--- test/bookingController.test.js
+++ test/bookingController.test.js
@@ -7,11 +7,12 @@ const {
   databaseDisconnector,
 } = require('../src/database');
 const {deleteUserByEmail} = require('../src/functions/userFunctions');
-const {createRoom} = require('../src/functions/roomFunctions');
+const {createBooking} = require('../src/functions/bookingFunctions');
 const {
   generateAccessCode,
   createSpace,
 } = require('../src/functions/spaceFunctions');
+const {createRoom} = require('../src/functions/roomFunctions')
 
 // Ensure the database is connected before all tests
 beforeAll(async () => {
@@ -19,7 +20,7 @@ beforeAll(async () => {
   await databaseConnector(databaseURL);
 });
 
-// disconnect after tests
+// Disconnect after tests
 afterAll(async () => {
   await databaseDisconnector();
 });
@@ -29,20 +30,20 @@ beforeEach(async () => {
   const delayDuration = 2000; // implement timeout
   await new Promise((resolve) => setTimeout(resolve, delayDuration));
   // Delete user before each test
-  const emailsToDelete = ['john.bobson@example.com'];
-  for (email of emailsToDelete) {
+  const emailsToDelete = ['ada.lovelace@example.com'];
+  for (const email of emailsToDelete) {
     await deleteUserByEmail(email);
   }
 }, 10000);
 
-describe('Room Router', () => {
-  describe('GET /rooms', () => {
-    test('should return an array of rooms', async () => {
+describe('Booking Router', () => {
+  describe('GET /bookings', () => {
+    test('should return an array of bookings', async () => {
       // Register a user and create a space for testing
       const registerResponse = await request(app).post('/users/register').send({
-        first_name: 'John',
-        last_name: 'Bobson',
-        email: 'john.bobson@example.com',
+        first_name: 'Ada',
+        last_name: 'Lovelace',
+        email: 'ada.lovelace@example.com',
         password: 'password123',
         post_code: '54321',
         country: 'NZ',
@@ -51,28 +52,28 @@ describe('Room Router', () => {
 
       // Log in the user and get the JWT
       const loginResponse = await request(app).post('/users/login').send({
-        email: 'john.bobson@example.com',
+        email: 'ada.lovelace@example.com',
         password: 'password123',
       });
 
       const jwt = await loginResponse.body.jwt;
 
       // Make a request to the endpoint with the JWT in the headers
-      const response = await request(app).get('/rooms').set('jwt', jwt);
+      const response = await request(app).get('/bookings').set('jwt', jwt);
 
       // Assertions
       expect(response.status).toBe(200);
-      expect(response.body.roomCount).toBeGreaterThanOrEqual(0);
-      expect(response.body.rooms).toBeInstanceOf(Array);
+      expect(response.body.bookingCount).toBeGreaterThanOrEqual(0);
+      expect(response.body.bookings).toBeInstanceOf(Array);
     });
   });
 
-  describe('GET /rooms/:roomID', () => {
-    test('should return details of a specific room', async () => {
+  describe('GET /bookings/:bookingID', () => {
+    test('should return details of a specific booking', async () => {
       const registerResponse = await request(app).post('/users/register').send({
-        first_name: 'John',
-        last_name: 'Bobson',
-        email: 'john.bobson@example.com',
+        first_name: 'Ada',
+        last_name: 'Lovelace',
+        email: 'ada.lovelace@example.com',
         password: 'password123',
         post_code: '54321',
         country: 'NZ',
@@ -85,7 +86,7 @@ describe('Room Router', () => {
         name: 'Test Space',
         description: 'Test space description',
         capacity: 10,
-        invite_code: await generateAccessCode(), // Ensure you have the invite_code variable defined
+        invite_code: await generateAccessCode(),
       };
 
       const createdSpace = await createSpace(spaceDetails);
@@ -98,9 +99,21 @@ describe('Room Router', () => {
       };
 
       const createdRoom = await createRoom(roomDetails);
+
+      const bookingDetails = {
+        room_id: createdRoom._id.toString(),
+        primary_user_id: registerResponse.body.user._id,
+        title: 'Test Booking',
+        description: 'This is a test booking',
+        start_time: new Date(),
+        end_time: new Date(),
+      };
+
+      const createdBooking = await createBooking(bookingDetails);
+
       // Log in the user and get the JWT
       const loginResponse = await request(app).post('/users/login').send({
-        email: 'john.bobson@example.com',
+        email: 'ada.lovelace@example.com',
         password: 'password123',
       });
 
@@ -108,21 +121,21 @@ describe('Room Router', () => {
 
       // Make a request to the endpoint with the JWT in the headers
       const response = await request(app)
-        .get(`/rooms/${createdRoom._id.toString()}`)
+        .get(`/bookings/${createdBooking._id.toString()}`)
         .set('jwt', jwt);
 
       // Assertions
       expect(response.status).toBe(200);
-      expect(response.body.name).toBe('Testing room creation');
+      expect(response.body.title).toBe('Test Booking');
     });
   });
 
-  describe('POST /rooms', () => {
-    test('should create a new room', async () => {
+  describe('POST /bookings', () => {
+    test('should create a new booking', async () => {
       const registerResponse = await request(app).post('/users/register').send({
-        first_name: 'John',
-        last_name: 'Bobson',
-        email: 'john.bobson@example.com',
+        first_name: 'Ada',
+        last_name: 'Lovelace',
+        email: 'ada.lovelace@example.com',
         password: 'password123',
         post_code: '54321',
         country: 'NZ',
@@ -135,17 +148,10 @@ describe('Room Router', () => {
         name: 'Test Space',
         description: 'Test space description',
         capacity: 10,
-        invite_code: await generateAccessCode(), // Ensure you have the invite_code variable defined
+        invite_code: await generateAccessCode(),
       };
 
       const createdSpace = await createSpace(spaceDetails);
-
-      const loginResponse = await request(app).post('/users/login').send({
-        email: 'john.bobson@example.com',
-        password: 'password123',
-      });
-
-      const jwt = await loginResponse.body.jwt;
 
       const roomDetails = {
         space_id: createdSpace._id.toString(),
@@ -154,23 +160,40 @@ describe('Room Router', () => {
         capacity: 100,
       };
 
+      const createdRoom = await createRoom(roomDetails);
+
+      const loginResponse = await request(app).post('/users/login').send({
+        email: 'ada.lovelace@example.com',
+        password: 'password123',
+      });
+
+      const jwt = await loginResponse.body.jwt;
+
+      const bookingDetails = {
+        room_id: createdRoom._id.toString(),
+        title: 'Test Booking',
+        description: 'This is a test booking',
+        start_time: new Date(),
+        end_time: new Date(),
+      };
+
       // Make a request to the endpoint with the JWT in the headers
       const response = await request(app)
-        .post(`/rooms`)
+        .post(`/bookings`)
         .set('jwt', jwt)
-        .send(roomDetails);
+        .send(bookingDetails);
 
       // Assertions
       expect(response.status).toBe(201);
     });
   });
 
-  describe('PUT /rooms/:roomID', () => {
-    test('should update details of a specific room', async () => {
+  describe('PUT /bookings/:bookingID', () => {
+    test('should update details of a specific booking', async () => {
       const registerResponse = await request(app).post('/users/register').send({
-        first_name: 'John',
-        last_name: 'Bobson',
-        email: 'john.bobson@example.com',
+        first_name: 'Ada',
+        last_name: 'Lovelace',
+        email: 'ada.lovelace@example.com',
         password: 'password123',
         post_code: '54321',
         country: 'NZ',
@@ -183,7 +206,7 @@ describe('Room Router', () => {
         name: 'Test Space',
         description: 'Test space description',
         capacity: 10,
-        invite_code: await generateAccessCode(), // Ensure you have the invite_code variable defined
+        invite_code: await generateAccessCode(),
       };
 
       const createdSpace = await createSpace(spaceDetails);
@@ -196,36 +219,47 @@ describe('Room Router', () => {
       };
 
       const createdRoom = await createRoom(roomDetails);
+
+      const bookingDetails = {
+        primary_user_id: registerResponse.body.user._id,
+        room_id: createdRoom._id.toString(),
+        title: 'Test Booking',
+        description: 'This is a test booking',
+        start_time: new Date(),
+        end_time: new Date(),
+      };
+
+      const createdBooking = await createBooking(bookingDetails);
+
       // Log in the user and get the JWT
       const loginResponse = await request(app).post('/users/login').send({
-        email: 'john.bobson@example.com',
+        email: 'ada.lovelace@example.com',
         password: 'password123',
       });
 
       const jwt = await loginResponse.body.jwt;
-      const updatedRoomDetails = {
-        name: 'Updated - testing room creation',
-        description: 'Updated - this is a new room',
-        capacity: 101,
+      const updatedBookingDetails = {
+        title: 'Updated - Test Booking',
+        description: 'Updated - This is a test booking',
       };
       // Make a request to the endpoint with the JWT in the headers
       const response = await request(app)
-        .put(`/rooms/${createdRoom._id.toString()}`)
+        .put(`/bookings/${createdBooking._id.toString()}`)
         .set('jwt', jwt)
-        .send(updatedRoomDetails);
+        .send(updatedBookingDetails);
 
       // Assertions
       expect(response.status).toBe(200);
-      expect(response.body.capacity).toBe(101);
+      expect(response.body.title).toBe('Updated - Test Booking');
     });
   });
 
-  describe('DELETE /rooms/:roomID', () => {
-    test('should delete a specific room', async () => {
+  describe('DELETE /bookings/:bookingID', () => {
+    test('should delete a specific booking', async () => {
       const registerResponse = await request(app).post('/users/register').send({
-        first_name: 'John',
-        last_name: 'Bobson',
-        email: 'john.bobson@example.com',
+        first_name: 'Ada',
+        last_name: 'Lovelace',
+        email: 'ada.lovelace@example.com',
         password: 'password123',
         post_code: '54321',
         country: 'NZ',
@@ -238,7 +272,7 @@ describe('Room Router', () => {
         name: 'Test Space',
         description: 'Test space description',
         capacity: 10,
-        invite_code: await generateAccessCode(), // Ensure you have the invite_code variable defined
+        invite_code: await generateAccessCode(),
       };
 
       const createdSpace = await createSpace(spaceDetails);
@@ -251,9 +285,21 @@ describe('Room Router', () => {
       };
 
       const createdRoom = await createRoom(roomDetails);
+
+      const bookingDetails = {
+        primary_user_id: registerResponse.body.user._id,
+        room_id: createdRoom._id.toString(),
+        title: 'Test Booking',
+        description: 'This is a test booking',
+        start_time: new Date(),
+        end_time: new Date(),
+      };
+
+      const createdBooking = await createBooking(bookingDetails);
+
       // Log in the user and get the JWT
       const loginResponse = await request(app).post('/users/login').send({
-        email: 'john.bobson@example.com',
+        email: 'ada.lovelace@example.com',
         password: 'password123',
       });
 
@@ -261,12 +307,12 @@ describe('Room Router', () => {
 
       // Make a request to the endpoint with the JWT in the headers
       const response = await request(app)
-        .delete(`/rooms/${createdRoom._id.toString()}`)
+        .delete(`/bookings/${createdBooking._id.toString()}`)
         .set('jwt', jwt);
 
       // Assertions
       expect(response.status).toBe(200);
-      expect(response.body.message).toBe('Room deleted successfully');
+      expect(response.body.message).toBe('Booking deleted successfully');
     });
   });
 });
